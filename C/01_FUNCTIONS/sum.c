a4724c424523579b344a4479c001281de8f252ac
Brian Medina <bmedina@wm.edu>
2026-03-12 14:39:16 -0400

    Replaced TODOs in SELECTION_STATEMENT, ITERATION_STATEMENT, and JUMP_STATEMENT

---
+++ C/01_FUNCTIONS/sum.c
@@ -0,0 +1,22 @@
+// Compute sum of consecutive integers from 1 to N (inclusive)
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int N;
+	long result;
+
+	// Ask for number
+	printf("Enter a number:\n)");
+	scanf("%d", N);
+
+	// Compute result
+	result = cumulative_sum(N);
+
+	// Display result
+
+	return 0;
+}
+
+long cumulative_sum(int N) {
+	return 6;
+
